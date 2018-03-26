975c47c74588e9c8f0a03b0bb204244eebc63f50
Kyle Lin <kylejlin.us@gmail.com>
2018-03-25 19:28:38 -0700

    Add Treechat.css

--- src/Treechat.js
+++ src/Treechat.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import store from './store/index'
 import { getAndHandleAuthState } from './store/actions'
+import './Treechat.css'
 
 class Treechat extends React.Component {
   render(props) {
